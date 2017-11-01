b90022bb98e06389aed170082d7ee4f99fdd0bec
profh <profh@cmu.edu>
2017-11-01 14:09:13 -0400

    Changing strategies

--- lib/omniauth/strategies/medium.rb
+++ lib/omniauth/strategies/medium.rb
@@ -4,12 +4,15 @@ module OmniAuth
   module Strategies
     class Medium < OmniAuth::Strategies::OAuth2
       option :name, 'medium'
-      option :scope, 'basicProfile,publishPost'
+      option :scope, 'basicProfile'
 
       option :client_options, {
         site: 'https://api.medium.com/v1',
         authorize_url: 'https://medium.com/m/oauth/authorize',
-        token_url: "https://api.medium.com/v1/tokens"
+        token_url: "https://api.medium.com/v1/tokens",
+        state: 'dGhlcmUncyBubyBuZWVkIHRvIGZlYXIuLi4=',
+        response_type: 'code',
+        redirect_uri: 'http%3A%2F%2Ftests.dev%2Fauth%2Fmedium%2Fcallback'
       }
 
       uid { info["id"] }
