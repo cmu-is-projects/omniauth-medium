bdd122a9aa8e1ce72fe2bb351c2b4f17979b32d7
profh <profh@cmu.edu>
2017-11-01 14:33:47 -0400

    Revising the strategies more

--- lib/omniauth/strategies/medium.rb
+++ lib/omniauth/strategies/medium.rb
@@ -7,13 +7,11 @@ module OmniAuth
       option :scope, 'basicProfile'
 
       option :client_options, {
-        client_id: '534c7bfcf9b5',
         site: 'https://api.medium.com/v1',
         authorize_url: 'https://medium.com/m/oauth/authorize',
         token_url: "https://api.medium.com/v1/tokens",
         state: 'dGhlcmUncyBubyBuZWVkIHRvIGZlYXIuLi4=',
-        response_type: 'coder',
-        # redirect_uri: 'http%3A%2F%2Ftests.dev%2Fauth%2Fmedium%2Fcallback'
+        response_type: 'code',
         redirect_uri: 'http://tests.dev/auth/medium/callback'
       }
 
